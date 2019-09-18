a2fbc0903224f65c4f68f1b8208ea182f18f9ff3
Cyrille Rossant <rossant@users.noreply.github.com>
2019-09-18 13:21:20 +0200

    Add IBL task placeholders

--- iblpipe.py
+++ iblpipe.py
@@ -1,42 +1,22 @@
 """IBL pipeline.
 
-A task type is defined by:
+A task is defined by:
 
 - a name (also name of a Python function)
 - a list of relative paths (possibly with a *) to input files/directories (relative to the
   session directory)
 - a list of relative paths (possibly with a *) to output files/directories (relative to the
   session directory)
-- a list of task type that should follow the current task type (dependencies)
-- a default n_cpus
-- a default n_gpus
-- a default io_charge (integer between 0 and 100)
-- a default priority
+- a list of dependencies (other task names)
+- n_cpus
+- n_gpus
+- io_charge (integer between 0 and 100)
+- priority
 
-A task request is defined by:
+A scheduler (possibly based on dask) accepts a list of tasks and executes them
+on the available resources (CPUs, GPUs).
 
-- the task type's name
-- a list of absolute full input paths
-- a list of absolute full output paths
-- an optional overriding n_cpus
-- an optional overriding n_gpus
-- an optional overriding io_charge
-- an optional overriding priority
-
-A task run is defined by:
-
-- a task request
-- a current status
-- a started datetime
-- a completed datetime
-
-Task types are defined directly in the code.
-Task requests are created in Python with an API.
-A scheduler (possibly based on dask) accepts a list of task requests and executes them
-on the available resources (CPUs, GPUs). It creates and maintains a bunch of task runs.
-All task runs are persisted on a TSV log file.
-
-The list of required task requests is obtained by scanning the existing directory for
+The list of required tasks is obtained by scanning the existing directory for
 tasks that need to be run.
 
 """
@@ -45,97 +25,234 @@ tasks that need to be run.
 # Imports
 # -------------------------------------------------------------------------------------------------
 
+import datetime
+from itertools import chain
+from pathlib import Path
 import os
 import sys
 
 
 # -------------------------------------------------------------------------------------------------
-# Generic pipelining tools
+# Utils
 # -------------------------------------------------------------------------------------------------
 
-def files_exist(paths):
-    """Return whether a list of absolute paths exist. The paths may contain a wildcard."""
-    # TODO
+def get_full_paths(session_dir, path_templates):
+    """Return a list of paths matching the given templates in the session directory."""
+    return list(chain(*(session_dir.glob(tmp) for tmp in path_templates)))
 
 
-class TaskType:
+def all_file_globs_exist(session_dir, path_templates):
+    """Return whether all path templates (with a wildcard *) exist or not."""
+    return (
+        len(path_templates) > 0 and
+        0 not in [len(list(session_dir.glob(p))) for p in path_templates])
+
+
+STATUS_LIST = ('pending', 'processing', 'completed', 'error')
+
+
+def is_session_dir(path):
+    """Return whether a directory is a session directory or not."""
+    # TODO: improve how we determine whether a dir is a session dir or not.
+    return (Path(path) / 'raw_ephys_data').exists()
+
+
+def find_session_dirs(roots):
+    """Recursively find a list of session dirs in one or several root directories."""
+    for r in roots:
+        for subdir in Path(r).rglob('*'):
+            if subdir.is_dir() and is_session_dir(subdir):
+                yield subdir
+
+
+# -------------------------------------------------------------------------------------------------
+# Base task class
+# -------------------------------------------------------------------------------------------------
+
+class Task:
+    """Base class for the tasks."""
+
+    # To set in the child classes.
     name = None
     input_path_templates = ()
     output_path_templates = ()
     depends_on = ()
+
+    # Charge.
     n_cpus = 0
     n_gpus = 0
     io_charge = 0
     priority = None
+
+    # Task status.
+    # See also # https://distributed.dask.org/en/latest/scheduling-state.html#task-state
+    _status = 'pending'
+    started = None
+    completed = None
+
+    def __init__(self, session_dir, **kwargs):
+        self.__dict__.update(kwargs)
+        self.session_dir = Path(session_dir).resolve()
+        self.input_paths = get_full_paths(self.session_dir, self.input_path_templates)
+        if self.is_complete():
+            self.status = 'completed'
 
     def run(self):
         """To override."""
         pass
 
-    def check_completion(self, session_dir):
-        """Check whether a task has been completed in a given session directory.
-        By default, look whether the output files exist."""
+    @property
+    def status(self):
+        """Task status: one of %s.""" % ', '.join(STATUS_LIST)
+        return self._status
 
+    @status.setter
+    def status(self, status):
+        assert status in STATUS_LIST
+        self._status = status
 
-class TaskRequest:
-    task_type = None
-    input_paths = ()
-    output_paths = ()
-    n_cpus = 0
-    n_gpus = 0
-    io_charge = 0
-    priority = None
+    def all_input_files_exist(self):
+        """Return whether all input files exist so that the task can run."""
+        return all_file_globs_exist(self.session_dir, self.input_path_templates)
 
+    def all_output_files_exist(self):
+        """Return whether all output files have been created."""
+        return all_file_globs_exist(self.session_dir, self.output_path_templates)
 
-class TaskRun:
-    task_request = None
-    status = None
-    started = None
-    completed = None
+    def is_complete(self):
+        """Return whether the task is complete, by default, if all output files have been created.
+        This method can be overriden for custom checks that the task has successfully completed."""
+        return self.all_output_files_exist()
 
-
-def get_task_types():
-    """Return all registered task types, by looking at the classes deriving from the base class."""
-    # use metaclass
-
-
-def is_session_dir(path):
-    """Return whether a directory is a session directory or not."""
-
-
-def find_session_dirs(roots):
-    """Recursively find a list of session dirs in one or several root directories."""
-
-
-def missing_task_requests(session_dir):
-    """Create a list of task requests in a session directory, by scanning the directory and
-    finding which tasks have not run yet and thus have not yet created the output files."""
-
-
-def create_dask_pipeline(task_requests):
-    """Create a Dask pipeline from a list of task requests."""
-    pass
-
-
-def run_dask_pipeline(pipeline):
-    """Run a Dask pipeline on the local computer, using multiple processes."""
+    def __repr__(self):
+        return '<Task %s (%s) in %s>' % (self.name, self.status, self.session_dir)
 
 
 # -------------------------------------------------------------------------------------------------
 # IBL tasks
 # -------------------------------------------------------------------------------------------------
 
-class ExtractEphysTask(TaskType):
+class ExtractBehaviorTask(Task):
+    name = 'extract_behaviour'
+    output_path_templates = ('alf/_ibl_trials.*',)
+    n_cpus = 1
+    io_charge = 50
+    priority = 'high'
+
+
+class RegisterTask(Task):
+    name = 'register'
+    n_cpus = 1
+    io_charge = 10
+    priority = 'high'
+
+
+class CompressVideoTask(Task):
+    name = 'compress_avi'
+    input_path_templates = ('raw_video_data/*.avi',)
+    output_path_templates = ('raw_video_data/*.mp4',)
+    n_cpus = 4
+    io_charge = 50
+    priority = 'normal'
+
+
+class RawEphysQCTask(Task):
+    name = 'raw_ephys_qc'
+    input_path_templates = ('raw_ephys_data/probe*/*.ap.bin',)
+    output_path_templates = (
+        'raw_ephys_data/probe*/_spikeglx_ephysQcTime',
+        'raw_ephys_data/probe*/_spikeglx_ephysQcFreq')
+    n_cpus = 4
+    priority = 'normal'
+
+
+class ExtractEphysTask(Task):
     name = 'extract_ephys'
     input_path_templates = ('raw_ephys_data/probe*/*.ap.bin',)
-    output_path_templates = ('/alf/_ibl_trials.*', 'raw_ephys_data/probe*/_spikeglx_sync*')
+    output_path_templates = ('alf/_ibl_trials.*', 'raw_ephys_data/probe*/_spikeglx_sync*')
     n_cpus = 4
     io_charge = 50
     priority = 'high'
 
-    def run(self):
-        # TODO
-        pass
+    # def run(self):
+    #     p = self.session_dir / 'alf/_ibl_trials.a'
+    #     p.parent.mkdir(exist_ok=True, parents=True)
+    #     p.touch()
+
+
+class SpikeSortingTask(Task):
+    name = 'spike_sorting'
+    input_path_templates = ('raw_ephys_data/probe*/*.ap.bin',)
+    output_path_templates = ('raw_ephys_data/probe*/*.npy',)
+    n_gpus = 1
+    priority = 'high'
+
+
+class CompressEphysTask(Task):
+    name = 'compress_ephys'
+    input_path_templates = ('raw_ephys_data/probe*/*.ap.bin',)
+    output_path_templates = ('raw_ephys_data/probe*/*.ap.cbin',)
+    n_cpus = 1
+    priority = 'low'
+
+
+class CompressVideoEphysTask(Task):
+    name = 'compress_ephys_avi'
+    input_path_templates = ()
+    output_path_templates = ('raw_video_data/*.mp4',)
+    n_cpus = 4
+    io_charge = 50
+    priority = 'normal'
+
+
+class DLCTask(Task):
+    name = 'dlc'
+    input_path_templates = ('raw_video_data/*.mp4',)
+    output_path_templates = ('alf/_dlc_*',)
+    n_gpus = 1
+    priority = 'normal'
+    depends_on = ('compress_avi', 'compress_ephys_avi')
+
+
+class SpikeSortingQCTask(Task):
+    name = 'qc_spike_sorting'
+    n_cpus = 1
+    priority = 'high'
+    depends_on = ('spike_sorting',)
+
+
+class SpikeSortingMerge(Task):
+    name = 'merge_spike_sorting'
+    n_cpus = 1
+    priority = 'high'
+
+
+# -------------------------------------------------------------------------------------------------
+# Pipeline routines
+# -------------------------------------------------------------------------------------------------
+
+def all_task_classes():
+    """Return all task classes, i.e. all subclasses of the base Task class."""
+    return Task.__subclasses__()
+
+
+def missing_tasks(session_dir):
+    """Create a list of tasks in a session directory, by scanning the directory and
+    finding which tasks have not run yet and thus have not yet created the output files."""
+    for task_cls in all_task_classes():
+        task = task_cls(session_dir)
+        if task.status != 'completed':
+            yield task
+
+
+def create_dask_pipeline(task_requests):
+    """Create a Dask pipeline from a list of task requests."""
+    # TODO
+
+
+def run_dask_pipeline(pipeline):
+    """Run a Dask pipeline on the local computer, using multiple processes."""
+    # TODO
 
 
 # -------------------------------------------------------------------------------------------------
@@ -143,4 +260,5 @@ class ExtractEphysTask(TaskType):
 # -------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    pass
+    session_dir = 'data'
+    print(list(missing_tasks(session_dir)))
